a76ad7b2c5af0a0b785f2e4a93c93b95e6fac8f1
Unknown <smalltom70@hotmail.com>
2018-05-15 00:16:52 +0100

    Initial setup of Project and Cucumber runner

---
+++ src/test/java/com/AutomationWipro/CukesRunner.java
@@ -0,0 +1,9 @@
+package com.AutomationWipro;
+
+import cucumber.api.junit.*;
+import org.junit.runner.RunWith;
+@RunWith(Cucumber.class)
+@Cucumber.Options(
+        features={"src/test/resources"}
+)
+public class CukesRunner {}
